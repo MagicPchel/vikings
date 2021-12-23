62a7d2c21b5d6eb4248b8284268fce17bc39861d
MagicPchel <vadim444516@mail.ru>
2021-12-23 08:55:45 +0300

    init

---
+++ викинги/js/main.js
@@ -0,0 +1,27 @@
+$(function(){
+  /*
+    $('[data-fancybox]').fancybox({
+        youtube : {
+            controls : 0,
+            showinfo : 0
+        }
+    });
+    */
+    
+ $('.heroes__slider-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-text',
+    prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png"></button>',
+    nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png"></button>',
+    
+  });
+  $('.heroes__slider-text').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-img',
+    fade: true,
+    arrows:false
+  });
+});
+
